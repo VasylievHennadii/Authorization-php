5be18347b994a8cfe0889fcfdaf0a32b60055cd6
VasylievHennadii <vga.kha@gmail.com>
2020-07-23 15:45:30 +0300

    Authorization and Registration in PHP(validation and Ajax)

--- register.php
+++ register.php
@@ -10,13 +10,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Авторизация и регистрация</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
 
     <!-- Форма регистрации -->
 
-    <form action="vendor/signup.php" method="post" enctype="multipart/form-data">
+    <form>
         <label>ФИО</label>
         <input type="text" name="full_name" placeholder="Введите свое полное имя">
         <label>Логин</label>
@@ -29,18 +29,13 @@
         <input type="password" name="password" placeholder="Введите пароль">
         <label>Подтверждение пароля</label>
         <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
-        <button type="submit">Войти</button>
+        <button type="submit" class="register-btn">Зарегистрироваться</button>
         <p>
             У вас уже есть аккаунт? - <a href="/">авторизируйтесь</a>!
         </p>
-        <?php
-            if ($_SESSION['message']) {
-                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
-            }
-            unset($_SESSION['message']);
-        ?>
-        
+        <p class="msg none">Lorem ipsum.</p>
     </form>
-
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
--- vendor/signin.php
+++ vendor/signin.php
@@ -1,34 +1,61 @@
 <?php
 
-    session_start();
-    require_once 'connect.php';
+session_start();
+require_once 'connect.php';
 
-    $login = $_POST['login'];
-    $password = md5($_POST['password']);
+$login = $_POST['login'];
+$password = $_POST['password'];
 
-    $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
-    if (mysqli_num_rows($check_user) > 0) {
+$error_fields = [];
 
-        $user = mysqli_fetch_assoc($check_user);
+if ($login === '') {
+    $error_fields[] = 'login';
+}
 
-        $_SESSION['user'] = [
-            "id" => $user['id'],
-            "full_name" => $user['full_name'],
-            "avatar" => $user['avatar'],
-            "email" => $user['email']
-        ];
+if ($password === '') {
+    $error_fields[] = 'password';
+}
 
-        header('Location: ../profile.php');
+if (!empty($error_fields)) {
+    $response = [
+        "status" => false,
+        "type" => 1,
+        "message" => "Проверьте правильность полей",
+        "fields" => $error_fields
+    ];
 
-    } else {
-        $_SESSION['message'] = 'Не верный логин или пароль';
-        header('Location: ../index.php');
-    }
-    ?>
+    echo json_encode($response);
 
-<pre>
-    <?php
-    print_r($check_user);
-    print_r($user);
-    ?>
-</pre>
+    die();
+}
+
+$password = md5($password);
+
+$check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+if (mysqli_num_rows($check_user) > 0) {
+
+    $user = mysqli_fetch_assoc($check_user);
+
+    $_SESSION['user'] = [
+        "id" => $user['id'],
+        "full_name" => $user['full_name'],
+        "avatar" => $user['avatar'],
+        "email" => $user['email']
+    ];
+
+    $response = [
+        "status" => true
+    ];
+
+    echo json_encode($response);
+
+} else {
+
+    $response = [
+        "status" => false,
+        "message" => 'Не верный логин или пароль'
+    ];
+
+    echo json_encode($response);
+}
+?>
